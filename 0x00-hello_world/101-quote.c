4e2eecddc4d87710d6c3b774068cd4d91cc65666
Matthew Joy <joy.matthew09@icloud.com>
2022-09-08 12:23:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Prints sizes of various types on the computer compiled ans run on
+ *
+ * Return: 0
  */
 int main(void)
 {
-	write(STDOUT_FILENO, and that piece of art is useful
-			\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
