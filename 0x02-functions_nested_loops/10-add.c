6bf80d2f1d32efdfadc6cd387a9685fcd8431909
Matthew Joy <joy.matthew09@icloud.com>
2022-09-13 10:48:06 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: two integers arguments
+ * @b: two integers arguments
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
