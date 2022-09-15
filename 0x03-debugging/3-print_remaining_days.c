c0e71779ac83d29043f8b9dbf0feabcfc3352642
Matthew Joy <joy.matthew09@icloud.com>
2022-09-15 08:27:37 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,9 +19,9 @@ void print_remaining_days(int month, int day, int year)
 			day++;
 		}
 
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
-	}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+		}
 	else
 	{
 		if (month == 2 && day == 60)
@@ -34,5 +34,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-
 }
