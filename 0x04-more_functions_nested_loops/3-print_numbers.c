da4e5743149fc938894527574064614bc29f259c
Matthew Joy <joy.matthew09@icloud.com>
2022-09-16 08:06:37 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,11 @@
 
 /**
  * print_numbers - print nymbers from 0 to 9, followed by a new line
- *
- * Return: 0;
  */
 
 void print_numbers(void)
 {
-	int c;
-
-	c = '0';
+	int c = '0';
 
 	while (c <= 9)
 	{
