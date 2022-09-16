7be930fc56aaa604fa733fdfe49360e9f5e8c82e
Matthew Joy <joy.matthew09@icloud.com>
2022-09-16 08:55:34 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0 to 9
+ * print_most_numbers - print 0 to 9, skip 2 and 4
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char i;
 
