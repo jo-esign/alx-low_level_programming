955160c8f1c39ef18256f2fb943136fc8029d036
Matthew Joy <joy.matthew09@icloud.com>
2022-09-16 09:41:54 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print character "\" n times
+ * @n: number of characters to draw
+ */
+
+void print_diagonal(int n)
+{
+	int line;
+	int space;
+
+	if (n > 0)
+	{
+		for (line = 1; line <= n; line++)
+		{
+			for (space = 1; space < line; space++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
