39af4eb537d976727e3ae472b21544690e2e18f6
Matthew Joy <joy.matthew09@icloud.com>
2022-09-16 11:48:44 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints 1 to 100
- * multiples of 3 prints Fizz
- * multiples of 5 prints Buzz
- * multiples of 3 and 5 prints FizzBuzz
- * Return: 0
+ * for multiples of 3 print Fizz
+ * for multiples of 5 print Buzz
+ * for multiples of both 3 and 5, print FizzBuzz
+ * Return: 0 (success)
  */
 
 int main(void)
@@ -14,22 +14,18 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
 		else
 			printf("%d", i);
 
 		if (i < 100)
 			printf(" ");
 	}
-
 	printf("\n");
-
-	return (0);
 }
-
 
