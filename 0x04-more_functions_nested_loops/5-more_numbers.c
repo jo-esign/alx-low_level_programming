3363419996960ba6dcce6c90332291758ea7fd07
Matthew Joy <joy.matthew09@icloud.com>
2022-09-16 09:14:07 -0700

    complete

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,9 @@ void more_numbers(void)
 	{
 		for (num = '0'; num <= '14'; num++)
 		{
-			_putchar(num);
+			if (num / 10 > 0)
+				_putchar((num / 10 + '0'));
+			_putchar((num % 10 + '0'));
 		}
 	}
 	_putchar ('\n');
