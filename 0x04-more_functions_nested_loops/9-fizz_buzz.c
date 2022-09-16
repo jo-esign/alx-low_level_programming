b180ec0a39c8f58bda61ffd2d07b1793430d5f85
Matthew Joy <joy.matthew09@icloud.com>
2022-09-16 10:05:50 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints FizzBuzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz \n");
+		else if ((i % 5) == 0)
+			printf("Buzz \n");
+		else if ((i % 15) == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d\n", i);
+	}
+	return (0);
+}
+
+
