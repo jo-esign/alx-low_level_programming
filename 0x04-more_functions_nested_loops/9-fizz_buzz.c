5d500eb4ffd32aa6bca7568d5e2894d7eadd24d1
Matthew Joy <joy.matthew09@icloud.com>
2022-09-16 10:16:00 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz \n");
+			printf("Fizz\n");
 		else if ((i % 5) == 0)
-			printf("Buzz \n");
+			printf("Buzz\n");
 		else if ((i % 15) == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz\n");
 		else
 			printf("%d\n", i);
 	}
-	return 0;
+	return (0);
 }
 
 
