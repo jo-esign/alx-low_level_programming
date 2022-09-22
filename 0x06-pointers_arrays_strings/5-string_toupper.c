306779e25370cd987987c900bb2f55ababf61e20
Matthew Joy <joy.matthew09@icloud.com>
2022-09-22 09:51:01 -0700

    complete

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @str: string to modify
+ * Return: uppercase
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (int)str[i] - 32;
+	}
+	return (str);
+}
