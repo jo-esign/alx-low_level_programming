b53d0de993d4d6caf9f5bfc5a9e7185cab7becb5
Jo <69627271+jo-esign@users.noreply.github.com>
2022-10-04 18:58:01 +0100

    add

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @src: string to append
+ * @dest: string to be overwritten
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = src[j];
+
+	return (dest);
+}
