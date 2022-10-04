51ef87a70835711dbb06f2612a013f82de81bd4e
Jo <69627271+jo-esign@users.noreply.github.com>
2022-10-04 18:59:14 +0100

    add

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @src: string to append
+ * @dest: string to concatenate to
+ * @n: number of interations
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] != 0 && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
+
