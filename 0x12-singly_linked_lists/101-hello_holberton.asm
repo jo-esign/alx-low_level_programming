a3c8c009acb13f4533c8912dde4a1d728a784f69
Matthew Joy <joy.matthew09@icloud.com>
2022-10-21 10:22:28 -0700

    God abeg

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,18 @@
-         global     main
-         extern    printf
+extern printf
 
+section .data
+msg:          db "Hello, Holberton", 0
+fmt:          db "%s", 10, 0
 
-         section    .text
+section .text
+
+global main
 main:
-         mov    rdi, message
-         call    printf
-         ret
-message:
-        db     "Hello, Holberton", 0
+push  rbp
+mov   rdi,fmt
+mov   rsi,msg
+mov   rax,0
+call  printf
+pop   rbp
+mov   rax,0
+ret
